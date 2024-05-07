e20b74d1898b92d143cab12c335c3cbf6cc9b61b
Ore <orellanojoaquinmarcelo@gmail.com>
2024-05-07 10:24:52 -0300

    add create and update schools

---
+++ BackEnd/Kronos/Kronosapp/serializers/school_serializer.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from ..models import School, ContactInformation, CustomUser
+
+
+class ContactInformationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactInformation
+        exclude = ['id']
+
+
+class DirectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'gender', 'email']
+
+
+class ReadSchoolSerializer(serializers.ModelSerializer):
+    contactInfo = ContactInformationSerializer()
+    directives = DirectiveSerializer(many=True)
+
+    class Meta:
+        model = School
+        fields = ['name', 'abbreviation', 'logo', 'email', 'directives', 'contactInfo']
+
+
+class IdDirectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id'] 
+
+
+class CreateSchoolSerializer(serializers.ModelSerializer):
+    contactInfo = ContactInformationSerializer()
+
+    class Meta:
+        model = School
+        fields = ['name', 'abbreviation', 'logo', 'email', 'directives', 'contactInfo']
+
+    def create(self, validated_data):
+        contact_info_data = validated_data.pop('contactInfo')
+        directives_data = validated_data.pop('directives', [])
+
+        contact_info = ContactInformation.objects.create(**contact_info_data)
+        school = School.objects.create(contactInfo=contact_info, **validated_data)
+
+        school.directives.set(directives_data)
+
+        return school
+
