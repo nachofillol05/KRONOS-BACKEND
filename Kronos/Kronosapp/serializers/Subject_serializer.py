cf406a0de04dbae7e1d42eb9fb5131d162287c94
derek7018 <derek198miller@gmail.com>
2024-08-13 11:35:13 -0300

    cambio en subject_serializer, ahora cursos esta nested

--- Kronos/Kronosapp/serializers/Subject_serializer.py
+++ Kronos/Kronosapp/serializers/Subject_serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models import Subject
+from .course_serializer import CourseSerializer
 
 class SubjectSerializer(serializers.ModelSerializer):
+    course = CourseSerializer()
     class Meta:
         model = Subject
         fields = ['id', 'name', 'studyPlan', 'description', 'weeklyHours', 'color', 'abbreviation', 'course']
