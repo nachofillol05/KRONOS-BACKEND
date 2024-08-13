19f3c5641b6ee6a09355130e8a1d24cfabd4a339
Teo Abril <teo.abril10@gmail.com>
2024-08-13 11:32:20 -0300

    fix course serailizer

--- Kronos/Kronosapp/serializers/course_serializer.py
+++ Kronos/Kronosapp/serializers/course_serializer.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from  .year_serializer import YearSerializer
-from ..models import Course
+from ..models import Course, Year
+
+class YearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Year
+        fields = ['id','name', 'description', 'number']
 
 class CourseSerializer(serializers.ModelSerializer):
     year = YearSerializer(many=False)
